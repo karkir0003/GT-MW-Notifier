6021ac5abf8058e0954ea646c7d2f9e10ca0896c
Ishan Baniya <ibaniya98@gmail.com>
2021-01-03 14:13:13 -0500

    Add custom form to add subscriber to mailchimp with recaptcha

--- mailchimp.py
+++ mailchimp.py
@@ -9,49 +9,56 @@ mailchimp_config = config['MAILCHIMP']
 
 # API Reference https://github.com/mailchimp/mailchimp-marketing-python
 
+
 class OnCampusJobList():
     def __init__(self):
         self._client = self.get_mailchimp_client()
         self._info = None
         self._members = None
-                
-    def get_mailchimp_client(self) -> MailchimpMarketing.Client:
-        try:
-            client = MailchimpMarketing.Client()
-            client.set_config({
-                "api_key": mailchimp_config['MAILCHIMP_API_KEY'],
-                "server": mailchimp_config['MAILCHIMP_SERVER_PREFIX']
-            })
-            return client        
 
-        except ApiClientError as error:
-            print(error)
-            return None
+    def get_mailchimp_client(self) -> MailchimpMarketing.Client:
+        client = MailchimpMarketing.Client()
+        client.set_config({
+            "api_key": mailchimp_config['MAILCHIMP_API_KEY'],
+            "server": mailchimp_config['MAILCHIMP_SERVER_PREFIX']
+        })
+        return client
+
+    def get_list_id(self):
+        return self.get_info()['id']
 
     def get_info(self):
         if self._info is None:
-            mailchimp_lists = self._client.lists.get_all_lists()['lists']    
-            filter_lambda = lambda x: x['name'] == mailchimp_config['LIST_NAME']
-            gt_on_campus_list = list(filter(filter_lambda, mailchimp_lists))
+            mailchimp_lists = self._client.lists.get_all_lists()['lists']
+            def custom_filter_lambda(
+                x): return x['name'] == mailchimp_config['LIST_NAME']
+            gt_on_campus_list = list(
+                filter(custom_filter_lambda, mailchimp_lists))
             if len(gt_on_campus_list) == 1:
-                self._info = gt_on_campus_list[0]                
+                self._info = gt_on_campus_list[0]
             else:
                 raise f"Failed to find a unique list for '{GT_ON_CAMPUS_LIST_ID}'"
-       
+
         return self._info
 
     def get_members(self):
         if self._members is None:
-            list_id = self.get_info()['id']
-            self._members = self._client.lists.get_list_members_info(list_id)['members']
-        
+            list_id = self.get_list_id()
+            self._members = self._client.lists.get_list_members_info(list_id)[
+                'members']
+
         return self._members
 
-def get_email_addresses():
+    def add_list_member(self, email):
+        list_id = self.get_list_id()
+        params = {
+            'email_address': email,
+            'status': 'subscribed'
+        }
+        return self._client.lists.add_list_member(list_id, body=params)
+
+
+if __name__ == "__main__":
     custom_list = OnCampusJobList()
     members = custom_list.get_members()
-    return [x['email_address'] for x in members]
-
-print(get_email_addresses())
-
-
+    print([x['email_address'] for x in members])
